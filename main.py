b3dddb977dd991b4909f96ed4db543bb4a2e9e1f
vujosp <vujos@wonderdynamics.com>
2021-08-16 17:58:27 +0200

    Addes support for exporting dataset with min seq sizes, so Marko can use them for transformer

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def run_async_mpt(num_of_threads, glob_query):
         tasks.append(in_thread(functools.partial(run_mpt, img_folder)))
     await asyncio.gather(*tasks)
 
-def export_as_ds(output, opt_path, kps_path, images, prefix_to_remove=None):
+def export_as_ds(output, opt_path, kps_path, images, prefix_to_remove=None, seq_size=None):
     tracking_results = np.load(os.path.join(images, 'tracking_results.pickle'), allow_pickle=True)
     imgnames_, parts_, centers_, scales_, pose_, shape_ = [], [], [], [], [], []
     image_paths = glob.glob(os.path.join(images, "*.png"))
@@ -77,8 +77,24 @@ def export_as_ds(output, opt_path, kps_path, images, prefix_to_remove=None):
 
         good_indices = np.delete(np.arange(kps.shape[0]), np.where(np.sum(kps[..., 2],axis=1) == 0)[0]) # From all possible indices, delete those where sum of confidence is equal to 0
         
-        if good_indices.shape[0] < 30:
+        if good_indices.shape[0] < 32:
             continue
+
+        if seq_size:
+            good_seq_images = []
+            holes = np.nonzero(np.diff(good_indices)-1)
+            holes = np.insert(holes, 0, 0) # Insert 0 at the begining
+            holes = np.insert(holes, len(holes), len(good_indices))
+            for i in range(len(holes)-1):
+                valid_elems = holes[i+1] - holes[i]
+                for j in range(int(np.floor(valid_elems/seq_size))-1):
+                    if j < 0:
+                        print(a)
+                    good_seq_images.append(good_indices[holes[i]+seq_size*j:holes[i]+seq_size*(j+1)])
+                    shape = good_seq_images[-1].shape[0]
+            if len(good_seq_images) == 0:
+                continue
+            good_indices = np.concatenate(good_seq_images, axis=0)
 
         p_tracking_res = tracking_results[int(p_id)]
         
@@ -165,13 +181,10 @@ def main(args):
         #     sub_call(f"blender {blender_output} --background --python output_to.py -- --keep_scene --fbx_file male --cam_type PERSP --name {p_id}_optim --background_image='{image_folder}' --npz_file '{os.path.join(opt_output, p_id + '_blender_params_optimization.npz')}' --blender_file_path '{blender_output}'", os.path.dirname(sdg_path))
         #     os.remove(blender_output + "1")
 
-        try:
-        #     export_as_ds(os.path.join(output_folder, 'dataset'), opt_output, kps_final, args.image_folder, '/mnt/nas2/datasets/3dpose_datasets/monoperfcap_test/')
-            all_datasets.append(os.path.join(output_folder, 'dataset.npz'))
-        except Exception as e:
-            print(e)
 
-    #     exit()
+        export_as_ds(os.path.join(output_folder, 'dataset'), opt_output, kps_final, args.image_folder, '/mnt/nas2/datasets/3dpose_datasets/monoperfcap_test/', 32)
+        all_datasets.append(os.path.join(output_folder, 'dataset.npz'))
+            
     merge_npz(os.path.join(args.output, 'datasets.npz'), all_datasets)
 
 if __name__ == "__main__":
